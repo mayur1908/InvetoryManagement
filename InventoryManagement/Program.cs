18c4cf4fd11379c7348dae3344e51fdd7fb10889
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-13 10:49:35 +0530

    added calculate rates

--- InventoryManagement/Program.cs
+++ InventoryManagement/Program.cs
@@ -15,7 +15,29 @@ namespace InvetoryManagement
                 Console.WriteLine(data.typesOfRice[i].name);
                 Console.WriteLine(data.typesOfRice[i].weight);
                 Console.WriteLine(data.typesOfRice[i].price);
-              
+                int invetoryprice = data.typesOfRice[i].weight * data.typesOfRice[i].price;
+                Console.WriteLine("The Price {0} {1}kg is {2}rs  ", data.typesOfRice[i].name, data.typesOfRice[i].weight, data.typesOfRice[i].price, invetoryprice);
+            }
+            Console.WriteLine("================== Types Of wheate  =================");
+            for (int i = 0; i < data.typesOfWheate.Count; i++)
+            {
+                Console.WriteLine(data.typesOfWheate[i].name);
+                Console.WriteLine(data.typesOfWheate[i].weight);
+                Console.WriteLine(data.typesOfWheate[i].price);
+                int invetoryprice = data.typesOfWheate[i].weight * data.typesOfWheate[i].price;
+
+                Console.WriteLine("The Price {0} {1}kg is {2}rs  ", data.typesOfWheate[i].name, data.typesOfWheate[i].weight, data.typesOfWheate[i].price, invetoryprice);
+
+            }
+            Console.WriteLine("================== Types Of pulses  =================");
+            for (int i = 0; i < data.typesOfPulses.Count; i++)
+            {
+                Console.WriteLine(data.typesOfPulses[i].name);
+                Console.WriteLine(data.typesOfPulses[i].weight);
+                Console.WriteLine(data.typesOfPulses[i].price);
+                int invetoryprice = data.typesOfPulses[i].weight * data.typesOfPulses[i].price;
+                Console.WriteLine("The Price {0} {1}kg is {2}rs  ", data.typesOfPulses[i].name, data.typesOfPulses[i].weight, data.typesOfPulses[i].price, invetoryprice);
+
             }
 
         }
