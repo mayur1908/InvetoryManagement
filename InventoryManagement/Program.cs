40c6a828669422b3fe2adb717befc1ddafa277c8
Mayur Sonkusare <mayursonkusare1155@gmail.com>
2023-05-13 10:43:00 +0530

    added program file

---
+++ InventoryManagement/Program.cs
@@ -0,0 +1,23 @@
+﻿using InventoryManagement;
+
+namespace InvetoryManagement 
+{
+    class program
+    {
+        public static void Main(string[] args) 
+        {
+            string path = "D:\\bridgelabz\\InventoryManagement\\InventoryManagement\\jsconfig1.json";
+            ReadTheData readTheData = new ReadTheData();
+            InventoryDetails data =  readTheData.Read(path);
+            Console.WriteLine("================== Types Of Rice  =================");
+            for (int i =0;i<data.typesOfRice.Count;i++)
+            {
+                Console.WriteLine(data.typesOfRice[i].name);
+                Console.WriteLine(data.typesOfRice[i].weight);
+                Console.WriteLine(data.typesOfRice[i].price);
+              
+            }
+
+        }
+    }
+}
